15f8319d5095035d725c0f37e8d8a5e176bb8a9a
Polad <bakhip@usi.ch>
2024-10-16 01:54:40 +0200

    btc trade history defined to handle incoming websocket messages

--- websocket.py
+++ websocket.py
@@ -3,3 +3,14 @@ from time import sleep
 from binance import ThreadedWebsocketManager
 
 btc_price = {'error': False}
+
+def btc_trade_history(msg):
+    """Define how to process incoming WebSocket messages"""
+    if msg['e'] != 'error':
+        print(msg['c'])
+        btc_price['last'] = msg['c']
+        btc_price['bid'] = msg['b']
+        btc_price['last'] = msg['a']
+        btc_price['error'] = False
+    else:
+        btc_price['error'] = True
