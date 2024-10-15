c130e994ab915ded70203ab8c4d8f8d8874fe66d
Polad <bakhip@usi.ch>
2024-10-15 22:13:08 +0200

    binance testnet api keys setup and replacing url to testnet url

--- balances.py
+++ balances.py
@@ -1,3 +1,12 @@
 import os
 
 from binance.client import Client
+
+# credentials
+test_api_key = os.environ.get('binance_test_api')
+test_api_secret = os.environ.get('binance_test_secret')
+
+client = Client(test_api_key, test_api_secret)
+
+client.API_URL = 'https://testnet.binance.vision/api'
+
