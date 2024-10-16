31b5d59e35df8c5672bc96428cf566b39adb0261
Polad <bakhip@usi.ch>
2024-10-17 00:32:29 +0200

    earliest timestamp fetching and printing

--- btc_historical_csv.py
+++ btc_historical_csv.py
@@ -9,3 +9,7 @@ test_api_key = os.environ.get('binance_test_api')
 test_api_secret = os.environ.get('binance_test_secret')
 
 client = Client(test_api_key, test_api_secret)
+
+# valid intervals - 1m, 3m, 1h, 12h, 1d, 1M
+timestamp = client._get_earliest_valid_timestamp('BTCUSDT', '1d')
+print(timestamp)
