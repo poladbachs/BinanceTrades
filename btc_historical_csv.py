309a5f7c3f94fbff82888eb8c8bfb5784ef0ea06
Polad <bakhip@usi.ch>
2024-10-17 00:41:44 +0200

    timestamp is now human readable, earlist BTCUSDT data with daily interval is shown

--- btc_historical_csv.py
+++ btc_historical_csv.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import json
+from datetime import datetime
 
 import pandas as pd
 from binance.client import Client
@@ -12,4 +13,7 @@ client = Client(test_api_key, test_api_secret)
 
 # valid intervals - 1m, 3m, 1h, 12h, 1d, 1M
 timestamp = client._get_earliest_valid_timestamp('BTCUSDT', '1d')
-print(timestamp)
+date = datetime.utcfromtimestamp(timestamp/1000)
+print(date)
+
+bars = client.get_historical_klines
