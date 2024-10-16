4ead016167d9fcdd29095e5c6d97a24cbfc9c5bb
Polad <bakhip@usi.ch>
2024-10-16 03:03:25 +0200

    BTC and ETH latest prices are arriving through websocket threads

--- websocket.py
+++ websocket.py
@@ -7,7 +7,17 @@ btc_price = {'error': False}
 def btc_trade_history(msg):
     """Define how to process incoming WebSocket messages"""
     if msg['e'] != 'error':
-        print(msg['c'])
+        print('BTC:',msg['c'])
+        btc_price['last'] = msg['c']
+        btc_price['bid'] = msg['b']
+        btc_price['last'] = msg['a']
+        btc_price['error'] = False
+    else:
+        btc_price['error'] = True
+
+def eth_trade_history(msg):
+    if msg['e'] != 'error':
+        print('ETH:',msg['c'])
         btc_price['last'] = msg['c']
         btc_price['bid'] = msg['b']
         btc_price['last'] = msg['a']
@@ -16,6 +26,14 @@ def btc_trade_history(msg):
         btc_price['error'] = True
 
 # init and start WebSocket
-bsm = ThreadedWebsocketManager()
+bsm = ThreadedWebsocketManager(testnet=True)
 bsm.start()
 
+bsm.start_symbol_ticker_socket(callback=btc_trade_history, symbol='BTCUSDT')
+
+bsm.start_symbol_ticker_socket(callback=eth_trade_history, symbol='ETHUSDT')
+
+while True:
+    sleep(10)
+
+bsm.stop()
