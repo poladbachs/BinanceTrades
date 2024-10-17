7187a42aeec58402d4a8590229f69d813ac88cb9
Polad <bakhip@usi.ch>
2024-10-18 01:31:35 +0200

    bta library tech indicators: 30 and 20 days SMA calculated and printed

--- indicators.py
+++ indicators.py
@@ -10,6 +10,16 @@ print(btc_df.tail(5))
 mean = btc_df.close.tail(20).mean()
 
 max_btc_value = btc_df.loc['2024']['close'].max()
-print(max_btc_value)
 
 print(mean)
+print(max_btc_value)
+
+# bta-lib indicators 
+
+# library uses 30 day period moving average
+sma = btalib.sma(btc_df.close)
+print(sma.df.tail(5))
+
+# 20 day MA
+btc_df['sma'] = btalib.sma(btc_df.close, period=20).df
+print(btc_df.tail(5))
