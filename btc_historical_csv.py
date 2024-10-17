4bfed6ba5bfc4f057b5eb0d970f6ace387e61417
Polad <bakhip@usi.ch>
2024-10-17 02:47:33 +0200

    pandas dataframe created and exported it to csv, all bugs and prev options not needed

--- btc_historical_csv.py
+++ btc_historical_csv.py
@@ -16,24 +16,32 @@ timestamp = client._get_earliest_valid_timestamp('BTCUSDT', '1m')
 date = datetime.utcfromtimestamp(timestamp/1000)
 print(date)
 
-bars = client.get_historical_klines('BTCUSDT', '1m', timestamp, limit=1)
+bars = client.get_historical_klines('BTCUSDT', '1d', timestamp, limit=1000)
 # print(bars)
 
 # option 1 - save to file using json - list of lists
-with open('btc_bars.json', 'w') as e:
-    json.dump(bars, e)
+# with open('btc_bars.json', 'w') as e:
+#     json.dump(bars, e)
 
 # option 2 - save as CSV file using the csv writer library
-with open('btc_bars.csv', 'w', newline='') as f:
-    wr = csv.writer(f)
-    for line in bars:
-        wr.writerow(line)
+# with open('btc_bars.csv', 'w', newline='') as f:
+#     wr = csv.writer(f)
+#     for line in bars:
+#         wr.writerow(line)
 
 # option 3 - save as CSV file without library, only showing date, open, high, low, close
-with ('btc_bars2.csv', 'w') as d:
+with open('btc_bars2.csv', 'w') as d:
     for line in bars:
         d.write(f'{line[0]}, {line[1]}, {line[2]}, {line[3]}, {line[4]}\n')
 
 # delete redundant data - keep date, open, high, low, close
 for line in bars:
     del line[5:]
+
+# option 4 - Pandas DF and export to csv
+btc_df = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close'])
+btc_df['date'] = pd.to_datetime(btc_df['date'], unit='ms')
+btc_df.set_index('date', inplace=True)
+print(btc_df.head())
+
+btc_df.head().to_csv('btc_bars3.csv')
