ec7df8a1857033418cfcebdbac204724c6be150b
Polad <bakhip@usi.ch>
2024-10-18 01:18:48 +0200

    one mean average of last 20 rows calculated

--- indicators.py
+++ indicators.py
@@ -2,6 +2,11 @@ import btalib
 import pandas as pd
 
 btc_df = pd.read_csv('btc_bars3.csv', index_col=0)
+btc_df.index = pd.to_datetime(btc_df.index)
 
 btc_df['20sma'] = btc_df.close.rolling(20).mean()
 print(btc_df.tail(5))
+
+mean = btc_df.close.tail(20).mean()
+
+print(mean)
