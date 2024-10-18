0bffd8fd4b3eee0aa1a59ec1bbb11200b2bff775
Polad <bakhip@usi.ch>
2024-10-18 22:23:54 +0200

    get eth price, format it to pass binance filter rules and limit order placing at the price

--- create_order.py
+++ create_order.py
@@ -8,8 +8,6 @@ test_api_key = os.environ.get('binance_api')
 test_api_secret = os.environ.get('binance_secret')
 
 client = Client(test_api_key, test_api_secret)
-
-print(test_api_key, test_api_secret)
     
 buy_order_limit = client.create_test_order(
 	symbol='ETHUSDT',
@@ -25,3 +23,9 @@ exchange_info = client.get_exchange_info()
 for symbol in exchange_info['symbols']:
     if symbol['symbol'] == 'ETHUSDT':
         print(symbol['filters'])
+
+symbol_ticker = client.get_symbol_ticker(symbol="ETHUSDT")
+current_price = float(symbol_ticker['price'])
+
+price = round(current_price * 0.99, 2)
+print(f"Placing a limit order at: {price}")
