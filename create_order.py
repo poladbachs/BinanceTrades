a88dde8de1a82e9f32e8e0e1ebfc946966a35b26
Polad <bakhip@usi.ch>
2024-10-19 00:20:22 +0200

    different order placement and cancel are explored and done

--- create_order.py
+++ create_order.py
@@ -18,13 +18,19 @@ eth_price = float(ticker['price'])
 
 print(f"Current ETH price: {eth_price}")
 
-# buy_order = client.order_market_buy(symbol=symbol, quantity=quantity)
+buy_order = client.order_market_buy(symbol=symbol, quantity=quantity)
 
-# print(f"Buy market order done: {buy_order}")
+print(f"Buy market order done: {buy_order}")
 
 sell_order = client.order_market_sell(symbol=symbol, quantity=quantity)
 
 print(f"Sell market order done: {sell_order}")
+
+buy_limit = client.order_limit_buy(symbol=symbol, quantity=quantity, price=2646.20)
+
+print(f"Buy limit order done: {buy_limit}")
+
+cancel = client.cancel_order(symbol='ETHUSDT', orderId=buy_limit['orderId'])
 
 print(client.get_asset_balance(asset='ETH'))
 
