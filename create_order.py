b0a4442b4e519dd9bc5855c8388a416f7a96b104
Polad <bakhip@usi.ch>
2024-10-19 00:48:59 +0200

    all orders done

--- create_order.py
+++ create_order.py
@@ -19,19 +19,20 @@ eth_price = float(ticker['price'])
 print(f"Current ETH price: {eth_price}")
 
 # BUY MARKET ORDER
-# buy_order = client.order_market_buy(symbol=symbol, quantity=quantity)
-# print(f"Buy market order done: {buy_order}")
+buy_order = client.order_market_buy(symbol=symbol, quantity=quantity)
+print(f"Buy market order done: {buy_order}")
 
 # SELL MARKET ORDER
-# sell_order = client.order_market_sell(symbol=symbol, quantity=quantity)
-# print(f"Sell market order done: {sell_order}")
+sell_order = client.order_market_sell(symbol=symbol, quantity=quantity)
+print(f"Sell market order done: {sell_order}")
 
 # BUY LIMIT ORDER
-# buy_limit = client.order_limit_buy(symbol=symbol, quantity=quantity, price=2500)
-# print(f"Buy limit order done: {buy_limit}")
+buy_limit = client.order_limit_buy(symbol=symbol, quantity=quantity, price=2500)
+print(f"Buy limit order done: {buy_limit}")
 
 # CANCEL ORDER
-# cancel = client.cancel_order(symbol='ETHUSDT', orderId='8997607')
+cancel = client.cancel_order(symbol='ETHUSDT', orderId=buy_limit['orderId'])
+print (f"Cancel order done: {cancel}")
 
 print(client.get_asset_balance(asset='ETH'))
 
