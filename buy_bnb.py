ad5a7c8c84e05e27d10bac012ac31dd021190632
Polad <bakhip@usi.ch>
2024-10-19 17:37:24 +0200

    BNB BUY done if drops below min balance

--- buy_bnb.py
+++ buy_bnb.py
@@ -7,14 +7,9 @@ test_api_secret = os.environ.get('binance_test_secret')
 
 client = Client(test_api_key, test_api_secret, testnet=True)
 
-print(client.get_asset_balance('BNB'))
-
 bnb_price = client.get_symbol_ticker(symbol='BNBUSDT')
 
 print(bnb_price)
-
-min_balance = 1.2
-topup = 1.3
 
 def topup_bnb(min_balance: float, topup: float):
     ''' Topup BNB if it drops below min balance'''
@@ -23,4 +18,14 @@ def topup_bnb(min_balance: float, topup: float):
     if bnb_balance < min_balance:
         quantity = round(topup - bnb_balance, 5)
         print(quantity)
+        order = client.order_market_buy(symbol='BNBUSDT', quantity=quantity)
+        return order
     return False
+
+
+min_balance = 1.5
+topup = 1.6
+order = topup_bnb(min_balance, topup)
+print(f"BNB topup done: {order}")
+print(client.get_asset_balance('BNB'))
+print(client.get_asset_balance('USDT'))
