54c7845659da37af8b0dbe0158816966ea2d7895
Polad <bakhip@usi.ch>
2024-10-19 19:16:43 +0200

    BUY ETH IF BTC HITS 68K, printed executed order and balances

--- pairs_trade_price.py
+++ pairs_trade_price.py
@@ -28,9 +28,28 @@ bsm.start_symbol_ticker_socket(
 while not price['BTCUSDT']:
     sleep(0.1)
 
+print(client.get_asset_balance(asset='USDT'))
+
 while True:
     if price['error']:
         bsm.stop()
         sleep(2)
         bsm.start()
         price['error'] = False
+
+    else:
+        if price['BTCUSDT'] > 68000:
+            try:
+                order = client.order_market_buy(symbol='ETHUSDT', quantity=0.1)
+                print(f"BUY ETH IF BTC HITS 68,000: {order}")
+                break
+            except Exception as e:
+                print(e)
+
+    sleep(0.1)
+
+bnb_price = client.get_symbol_ticker(symbol='ETHUSDT')
+print(client.get_asset_balance(asset='ETH'))
+print(client.get_asset_balance(asset='USDT'))
+
+bsm.stop()
