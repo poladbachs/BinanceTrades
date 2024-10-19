6efd85a5dce8143a96bf0b83cd07565f2df84538
Polad <bakhip@usi.ch>
2024-10-19 17:19:21 +0200

    fetched BNB historical data to put reasonable min balance, started topup BNB

--- buy_bnb.py
+++ buy_bnb.py
@@ -7,5 +7,12 @@ test_api_secret = os.environ.get('binance_test_secret')
 
 client = Client(test_api_key, test_api_secret, testnet=True)
 
+btc_price = client.get_symbol_ticker(symbol='BNBUSDT')
 
+print(client.get_asset_balance('BNB'))
 
+min_balance = 0.1
+topup = 0.3
+
+def topup_bnb(min_balance: float, topup: float):
+    ''' Topup BNB if it drops below min balance'''
