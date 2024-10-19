0d2ecef0ef40d629631d157f484913fcb7eacedb
Polad <bakhip@usi.ch>
2024-10-19 20:58:49 +0200

    callback to populate BTC dataframe with prices

--- pairs_trade_percent.py
+++ pairs_trade_percent.py
@@ -8,4 +8,9 @@ from binance.client import Client
 futures_test_api_key = os.environ.get('binance_futures_test_api')
 futures_test_api_secret = os.environ.get('binance_futures_test_secret')
 client = Client(futures_test_api_key, futures_test_api_secret, testnet=True)
+
 price = {'BTCUSDT': pd.DataFrame(columns=['date', 'price']), 'error':False}
+
+def btc_pairs_trade(msg):
+    if msg['e'] != 'error':
+        price['BTCUSDT'].loc[len(price['BTCUSDT'])] = [pd.Timestamp.now(), float(msg['c'])]
